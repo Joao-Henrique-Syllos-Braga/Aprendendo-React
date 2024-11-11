35bc3563cc68df83ad4efa3e27b8d9df83199b13
Syllos <joaohenrique.braga@outlook.com>
2024-11-11 20:08:58 -0300

    Primeiros passos do site

---
+++ arquivo1/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />     
+      <h1>Hello World!</h1>
+      <p>Olá Mundo! Estou aprendendo React JS</p>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ arquivo1/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer () {
+    return (
+        <footer className={styles.footer}>
+            <p>Todos os direitos reservados &copy; 2022</p>
+        </footer>
+    )
+}
+
+export default Footer;
